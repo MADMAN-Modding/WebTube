4d39bbb755a2e9e0f49321b712dd4c3ecc1c5c21
MADMAN-Modding <mad@kmdtech.net>
2024-02-25 12:35:44 -0500

    testing

--- entrypointCommands.sh
+++ entrypointCommands.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 mkdir /var/www/localhost/htdocs/videos
-git pull && chmod 777 -R /var/www/localhost/htdocs/* && httpd -D FOREGROUND
+git pull
+chmod 777 -R /var/www/localhost/htdocs/*
+httpd -D FOREGROUND
