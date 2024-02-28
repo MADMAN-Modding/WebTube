4a9974382a337bb7fc29f5cbc6441eb36e081852
MADMAN-Modding <mad@kmdtech.net>
2024-02-27 20:54:14 -0500

    File size and some character stuff wahoo, time to study

--- index.php
+++ index.php
@@ -1,85 +1,82 @@
 <!DOCTYPE html>
-<meta name="viewport" content="width=device-width, initial-scale=1" charset="UTF-8" lang="en">
-
+<html lang="en">
 <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1" charset="UTF-8">
   <title>Web YT-DLP</title>
   <link type="text/css" rel="stylesheet" href="css/index.css" />
 </head>
 
 <body>
   <h1>Welcome to Web YT-DLP</h1>
-  </head>
+  <input type="text" name="url" id="downloader">
+  <p> Enter the url of the video</p>
+  <button onclick="fileDownload()">Download!</button>
+  <?php
 
-  <body>
-    <input type="text" name="url" id="downloader">
-    <p> Enter the url of the video</p>
-    <button onclick="fileDownload()">Download!</button>
-    <?php
-
-    // Change for your use and in fileHandler.php
-    // $dir = "/var/www/localhost/htdocs/videos";
-    $dir = "videos";
+  // Change for your use and in fileHandler.php
+  // $dir = "/var/www/localhost/htdocs/videos";
+  $dir = "videos";
 
 
-    // mkdir("videos");
-    mkdir($dir);
+  // mkdir("videos");
+  mkdir($dir);
 
-    // I know, I should filter
-    if (isset($_POST["url"])) {
-      exec("cd $dir && yt-dlp -f mp4 $_POST[url]");
-    }
-    if ($videoFinder = opendir($dir)) {
+  // I know, I should filter
+  if (isset($_POST["url"])) {
+    exec("cd $dir && yt-dlp -f mp4 $_POST[url]");
+  }
+  if ($videoFinder = opendir($dir)) {
 
-      // Makes the videpList array
-      $videoList = [];
+    // Makes the videpList array
+    $videoList = [];
 
-      // Reads the directory till the end
-      while (false !== ($video = readdir($videoFinder))) {
-        // Adds each video to the list
-        $videoList[] = $video;
-      }
-
-      // Alphabetically sorts the videos
-      natsort($videoList);
-
-      // Removes . and .. from the array
-      array_splice($videoList, 0, 2);
-
-      // Closes the directory to save resources
-      closedir($videoFinder);
+    // Reads the directory till the end
+    while (false !== ($video = readdir($videoFinder))) {
+      // Adds each video to the list
+      $videoList[] = $video;
     }
 
-    echo "<div id=\"videos\">";
+    // Alphabetically sorts the videos
+    natsort($videoList);
 
-    foreach ($videoList as $video) {
+    // Removes . and .. from the array
+    array_splice($videoList, 0, 2);
 
-      // Gets the file size in GB with two decimal places 
-      $fileSize = filesize("videos/$video");
-      if ($fileSize >= 1073741824) {
-        $fileSize = ((int) (($fileSize / 1073741824) * 100)) / 100;
-        $dataValue = "GB";
-      } else {
-        $fileSize = ((int) (($fileSize / 1048576) * 100)) / 100;
-        $dataValue = "MB";
-      }
+    // Closes the directory to save resources
+    closedir($videoFinder);
+  }
 
-      // Gets rid of illegal characters cause they're annoying
-      $illegalChars = array("'", '"', "&");
+  echo "<div id=\"videos\">";
 
-      $videoOldName = $video;
+  foreach ($videoList as $video) {
 
-      foreach($illegalChars as $char) {
-        $video = str_replace($char, "", $video);
-      }
-
-      rename("videos/$videoOldName", "videos/$video");
-      
-      echo "<h3 class=\"video\" id=\"$video\">$video | $fileSize $dataValue <a href=\"videos/$video\" download><img src=\"images/Download Button.png\" class=\"download\"/></a><img src=\"images/Trash Button.png\" id=\"delete\" class=\"delete\" onclick='fileDelete(\"$video\")'/></h3><br> ";
+    // Gets the file size in GB with two decimal places 
+    $fileSize = filesize("videos/$video");
+    if ($fileSize >= 1073741824) {
+      $fileSize = ((int) (($fileSize / 1073741824) * 100)) / 100;
+      $dataValue = "GB";
+    } else {
+      $fileSize = ((int) (($fileSize / 1048576) * 100)) / 100;
+      $dataValue = "MB";
     }
 
-    echo "</div>";
-    ?>
-  </body>
-  <script src="js/index.js"></script>
+    // Gets rid of illegal characters cause they're annoying
+    $illegalChars = array("'", '"', "&");
 
-  </html>
+    $videoOldName = $video;
+
+    foreach ($illegalChars as $char) {
+      $video = str_replace($char, "", $video);
+    }
+
+    rename("videos/$videoOldName", "videos/$video");
+
+    echo "<h3 class=\"video\" id=\"$video\">$video | $fileSize $dataValue <a href=\"videos/$video\" download><img src=\"images/Download Button.png\" class=\"download\"/></a><img src=\"images/Trash Button.png\" id=\"delete\" class=\"delete\" onclick='fileDelete(\"$video\")'/></h3><br> ";
+  }
+
+  echo "</div>";
+  ?>
+<script src="js/index.js"></script>
+</body>
+
+</html>
